06e8694a7ccef4d5bb92029bf1961b9e18b6c673
Marc Ho <marc.ho@etrove.ca>
2016-12-11 21:58:22 -0800

    Initial Commit

---
+++ TransitData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitData
+//
+//  Created by Marc Ho on 2016-12-11.
+//  Copyright © 2016 Marc Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
